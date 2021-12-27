555b49a3181548febbfc753dbbbbe534c6c4efcd
Heshan <heshan.imedha@gmail.com>
2021-12-27 15:47:19 +0530

    Provider Package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:brew_crew/screens/authenticate/wrapper.dart';
+import 'package:brew_crew/services/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,9 +16,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Wrapper(),
+    return StreamProvider<MyUser?>.value(
+      initialData: null,
+      value: AuthService().user,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Wrapper(),
+      ),
     );
   }
 }
--- lib/screens/authenticate/wrapper.dart
+++ lib/screens/authenticate/wrapper.dart
@@ -1,12 +1,17 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:brew_crew/screens/authenticate/authenticate.dart';
 import 'package:brew_crew/screens/authenticate/home/home.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<MyUser?>(context);
+    print(user);
+
     return Authenticate();
   }
 }
