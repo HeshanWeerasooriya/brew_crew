cbeb12d6de7584974fa6c0b5cf0293ef18eefba2
Heshan <heshan.imedha@gmail.com>
2021-12-27 16:31:12 +0530

    Sign Out

--- lib/screens/authenticate/home/home.dart
+++ lib/screens/authenticate/home/home.dart
@@ -1,10 +1,35 @@
-import 'package:flutter/cupertino.dart';
+import 'package:brew_crew/screens/authenticate/authenticate.dart';
+import 'package:brew_crew/services/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    return Text('Home');
+    return Container(
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text('Brew Crew'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text('logout'),
+              onPressed: () async {
+                await _auth.signOut();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Authenticate()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
